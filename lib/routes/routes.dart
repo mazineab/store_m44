314df8abe987206f7bf3bd2084265999f228ad20
mazineab <mazinabjlal@gmail.com>
2024-08-05 12:14:01 +0000

    routes v0.1

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,10 +1,12 @@
 import 'package:get/get.dart';
 import 'package:store_m44/features/home/bidings/home_bidings.dart';
+import 'package:store_m44/features/home/screens/categories_page.dart';
 import 'package:store_m44/features/home/screens/home.dart';
-import 'package:store_m44/routes/route_names.dart';
+import 'package:store_m44/routes/routes_names.dart';
 
 class Routes {
   static appRoutes()=>[
-    GetPage(name: RouteNames.home, page:()=>Home(),binding: HomeBidings())
+    GetPage(name: RoutesNames.home, page:()=>Home(),binding: HomeBidings()),
+    GetPage(name: RoutesNames.categories, page:()=>const CategoriesPage(),binding: HomeBidings()),
   ];
 }
