fb8fe76b27c6d9af3ad977096deba3ad11cdf8ae
mazineab <mazinabjlal@gmail.com>
2024-08-06 09:43:29 +0000

    tiktok scroller screen&controller

---
+++ lib/features/home/screens/tiktok_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:store_m44/global/widgets/custom_app_bar.dart';
+import 'package:store_m44/global/widgets/product_detail_widget.dart';
+import 'package:tiktoklikescroller/tiktoklikescroller.dart';
+
+import '../../../data/models/product.dart';
+
+class TiktokScreen extends StatelessWidget {
+  final List<Product> list;
+
+  TiktokScreen({super.key, required this.list});
+
+  @override
+  Widget build(BuildContext context) {
+    final Controller controller = Controller();
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: CustomAppBar(title: "Products"),
+      body: TikTokStyleFullPageScroller(
+        contentSize: list.length,
+        swipePositionThreshold: 0.2,
+        swipeVelocityThreshold: 2000,
+        animationDuration: const Duration(milliseconds: 400),
+        controller: controller,
+        builder: (context, index) {
+          if (index >= list.length) {
+            return Center(child: Text('No more products'));
+          }
+          Product product = list[index];
+          return ProductDetailWidget(product: product);
+        },
+      ),
+    );
+  }
+}
