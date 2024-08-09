eb20ffdd117f39ac9edfaba55bcd78e5b2e9f11a
mazineab <mazinabjlal@gmail.com>
2024-08-09 23:58:25 +0000

    create sort and listener

--- lib/features/home/controllers/search_controller.dart
+++ lib/features/home/controllers/search_controller.dart
@@ -12,6 +12,7 @@ class SearchScreenController extends GetxController{
   var listProduct=<Product>[].obs;
   var isload=true.obs;
 
+
   Future<void> loadAllProduct()async{
     var listCategory=Constant.list;
     listCategory.forEach((e)async{
@@ -21,24 +22,73 @@ class SearchScreenController extends GetxController{
     });
     await Future.delayed(const Duration(seconds:5));
     isload.value=false;
+    sortListOfProducts();
     update();
+  }
+
+  Future<void> loadProductsByCategory(String category)async{
+      var list=await productRepositories.loadData(category);
+      listProduct.assignAll(list.map((e)=>Product.fromJson(e)).toList());
+      sortListOfProducts();
+      update();
+  }
+
+  Future<void> loadProducts()async{
+    isload.value = true;
+    listProduct.clear();
+    if(controller.category.value!="All"){
+      await loadProductsByCategory(controller.category.value);
+    }else{
+      await loadAllProduct();
+    }
+    filterList.assignAll(listProduct);
+    isload.value = false;
+    update();
+
   }
 
   searchProduct(String word,String category){
     var seachBy=controller.selectSearch;
+    print(seachBy);
     if(seachBy.value=="Description"){
       filterList.assignAll(listProduct.where((e)=>e.description.toLowerCase().contains(word.toLowerCase())));
     }
     else if(seachBy.value=="Brand name"){
-    filterList.assignAll(listProduct.where((e)=>e.nameBrand.toLowerCase().contains(word.toLowerCase())));
+     filterList.assignAll(listProduct.where((e)=>e.nameBrand.toLowerCase().contains(word.toLowerCase())));
+    }
+    else if(seachBy.value=="Price"){
+      filterList.assignAll(listProduct.where((e)=>e.prix.replaceAll(" ","").toLowerCase().contains(word.toLowerCase())));
     }
     update();
+  }
+
+  sortListOfProducts(){
+    var sortBy=controller.selectSort;
+    print(sortBy);
+    if(sortBy.value=="Price"){
+      filterList.sort((a,b)=> parsePrice(a.prix).compareTo(parsePrice(b.prix)));
+    }
+    else if(sortBy.value=="Description"){
+      filterList.sort((a, b) => a.description.compareTo(b.description));
+    }else if(sortBy.value=="Brand name"){
+      filterList.sort((a, b) => a.nameBrand.compareTo(b.nameBrand));
+    }
+    else{
+      filterList.assignAll(listProduct);
+    }
+    update();
+  }
+
+  int parsePrice(String price){
+    return int.parse(price.replaceAll(" ",""));
   }
 
   @override
   void onInit()async{
     super.onInit();
-    await loadAllProduct();
-    filterList.assignAll(listProduct);
+    ever(controller.category,(_)=>loadProducts());
+    ever(controller.selectSort, (_) => sortListOfProducts());
+    loadProducts();
+    // filterList.assignAll(listProduct);
   }
 }
