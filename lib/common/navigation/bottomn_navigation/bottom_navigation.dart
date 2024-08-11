82683dd197989ce456c77a66bd7edcf59fe46ed4
mazineab <mazinabjlal@gmail.com>
2024-08-11 00:51:31 +0000

    add new bottomNav & badge cart

--- lib/common/navigation/bottomn_navigation/bottom_navigation.dart
+++ lib/common/navigation/bottomn_navigation/bottom_navigation.dart
@@ -1,8 +1,9 @@
+import 'package:flashy_tab_bar2/flashy_tab_bar2.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:icons_plus/icons_plus.dart';
 import 'package:store_m44/common/navigation/bottomn_navigation/bottom_navigation_controller.dart';
-
-import '../../../core/utils/my_color.dart';
+import 'package:badges/badges.dart' as badges;
 
 class BottomNavigation extends StatelessWidget {
   const BottomNavigation({super.key});
@@ -10,28 +11,38 @@ class BottomNavigation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetBuilder<BottomNavigationController>(
-      init: BottomNavigationController(),
-      builder: (controller){
-        return BottomNavigationBar(
-          backgroundColor:MyColors.bgColor,
-          items: const [
-            BottomNavigationBarItem(
+        init: BottomNavigationController(),
+        builder: (controller) {
+          return FlashyTabBar(
+            selectedIndex: controller.selctedIndex.value,
+            showElevation: true,
+            onItemSelected: controller.onTapItem,
+            items: [
+              FlashyTabBarItem(
                 icon: Icon(Icons.home),
-                label: "home"
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.view_headline),
-                label: "category"
-            ),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.shopping_cart_rounded),
-                label: "card"
-            )
-          ],
-          currentIndex: controller.selctedIndex.value,
-          onTap: controller.onTapItem,
-        );
-      }
-    );
+                title: Text('Home'),
+              ),
+              FlashyTabBarItem(
+                icon: Icon(FontAwesome.list_solid),
+                title: Text('Category'),
+              ),
+              FlashyTabBarItem(
+                icon: Obx(()=>
+                  badges.Badge(
+                    showBadge:controller.isempty.value,
+                    badgeContent: Text(
+                        '${controller.countProuct.value}',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    child: Icon(Icons.shopping_cart_rounded),
+                  ),
+                ),
+                title: Text('Cart'),
+              ),
+
+
+            ],
+          );
+        });
   }
 }
--- lib/common/navigation/bottomn_navigation/bottom_navigation_controller.dart
+++ lib/common/navigation/bottomn_navigation/bottom_navigation_controller.dart
@@ -1,10 +1,14 @@
 import 'package:get/get.dart';
+import 'package:store_m44/features/home/controllers/card_page_controller.dart';
 import 'package:store_m44/features/home/screens/card_page.dart';
 import 'package:store_m44/features/home/screens/categories_page.dart';
 import 'package:store_m44/features/home/screens/home.dart';
 
 class BottomNavigationController extends GetxController{
+  CardPageController cardPageController=Get.put(CardPageController());
   var selctedIndex=0.obs;
+  var countProuct=0.obs;
+  var isempty=false.obs;
   final List pages=[
     Home(),
     const CategoriesPage(),
@@ -16,10 +20,22 @@ class BottomNavigationController extends GetxController{
     update();
   }
 
+   chnageCounte(){
+    countProuct.value=cardPageController.productAdd.length;
+   }
+
+   changeval(){
+    isempty.value=countProuct.value!=0;
+   }
+
   @override
   void onInit() {
     super.onInit();
     onTapItem(selctedIndex.value);
+    cardPageController.productAdd.listen((_){
+      chnageCounte();
+      changeval();
+    });
   }
 
 }
